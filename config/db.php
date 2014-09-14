4f6f2c4db41d3e159acb62232fc454fe12dc3ede
Dan Stace <dstace@gmail.com>
2014-09-14 13:27:43 -0400

    Added development db config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,20 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=tokims.db.3180200.hostedresource.com;dbname=tokims',
-    'username' => 'tokims',
-    'password' => 'HFUpsavntE2N@9',
-    'charset' => 'utf8',
-];
+if ('prod' === YII_ENV)
+{
+	return [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=tokims.db.3180200.hostedresource.com;dbname=tokims',
+	    'username' => 'tokims',
+	    'password' => 'HFUpsavntE2N@9',
+	    'charset' => 'utf8',
+	];
+} else {
+	return [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=kimsdev.db.3180200.hostedresource.com;dbname=kimsdev',
+	    'username' => 'kimsdev',
+	    'password' => 'l@HVHatP!6LDHzm',
+	    'charset' => 'utf8',
+	];
+}
